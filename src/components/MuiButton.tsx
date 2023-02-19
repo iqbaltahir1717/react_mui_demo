32eb59edd707f00cc83311cf3bcbba5f13bbed1d
Iqbal Tahir <iqbalknightphantom33@gmail.com>
2023-02-19 21:25:41 +0800

    first commit

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,11 @@
+import { Stack, Button } from "@mui/material"
+
+export const MuiButton = () => {
+  return (
+    <Stack spacing={2} direction='row'>
+        <Button variant='text' href="https://google.com">Text</Button>
+        <Button variant='contained'>Contained</Button>
+        <Button variant='outlined'>Outlined</Button>
+    </Stack>
+  )
+}
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,20 @@
+import { Typography } from "@mui/material"
+
+export const MuiTypography = () => {
+  return (
+    <div>
+        <Typography variant='h1'>h1 heading</Typography>
+        <Typography variant='h2'>h2 heading</Typography>
+        <Typography variant='h3'>h3 heading</Typography>
+        <Typography variant='h4'>h4 heading</Typography>
+        <Typography variant='h5'>h5 heading</Typography>
+        <Typography variant='h6'>h6 heading</Typography>
+
+        <Typography variant='subtitle1'>Sub Title 1</Typography>
+        <Typography variant='subtitle2'>Sub Title 2</Typography>
+
+        <Typography variant='body1'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem optio rerum aut nisi, nemo assumenda dolores laboriosam in, omnis quae accusamus recusandae earum ipsum voluptatum dolorum doloribus ut ex quam!</Typography>
+        <Typography variant='body2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum sapiente dicta odit illo tempora enim nisi dolores quod omnis, placeat sequi dolor architecto magni optio iure libero voluptatibus pariatur fugiat.</Typography>
+    </div>
+  )
+}
