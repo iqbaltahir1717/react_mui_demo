41fa8d1cafbb3d0a79e81b7f71aca93f825f1a49
Iqbal Tahir <iqbalknightphantom33@gmail.com>
2023-02-19 21:49:27 +0800

    add change

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,11 +1,37 @@
-import { Stack, Button } from "@mui/material"
+import { Stack, Button, IconButton } from "@mui/material"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LoginIcon from '@mui/icons-material/Login';
 
 export const MuiButton = () => {
   return (
-    <Stack spacing={2} direction='row'>
-        <Button variant='text' href="https://google.com">Text</Button>
-        <Button variant='contained'>Contained</Button>
-        <Button variant='outlined'>Outlined</Button>
+    <Stack spacing={4}>
+        <Stack spacing={2} direction='row'>
+            <Button variant='text' href="https://google.com">Text</Button>
+            <Button variant='contained'>Contained</Button>
+            <Button variant='outlined'>Outlined</Button>
+        </Stack>
+        <Stack spacing={2} direction='row'>
+            <Button variant="outlined" color="primary" startIcon={<LoginIcon />} >Login</Button>
+            <Button variant="outlined" color="secondary">Selengkapnya</Button>
+            <Button variant="outlined" color="info">Info</Button>
+            <Button variant="text" color="success">Success</Button>
+            <Button variant="contained" color="error">Error</Button>
+        </Stack>
+        <Stack display='block' spacing={2} direction='row'>
+            <Button variant="contained" size='small'>Small</Button>
+            <Button variant="contained" size='medium'>Medium</Button>
+            <Button variant="contained" size='large'>Large</Button>
+        </Stack>
+        <Stack spacing={2} direction='row' display='block'>
+            <Button variant='text' startIcon={<AccountCircleIcon />}> Samuel R.</Button>
+            <Button variant='contained' endIcon={<AccountCircleIcon />}> Profile</Button>
+            <IconButton aria-label='login' size="small">
+                <LoginIcon /> 
+            </IconButton>
+            <IconButton aria-label='login' color="secondary" size="small">
+                <AccountCircleIcon /> 
+            </IconButton>
+        </Stack>
     </Stack>
   )
 }
